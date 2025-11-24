da93811162b764cc78e41faa0f97a2d280f7b3ae
shawn8989 <shunathon@icloud.com>
2025-11-24 14:08:02 -0800

    Initial Commit

---
+++ AirPad/AirPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPadApp.swift
+//  AirPad
+//
+//  Created by shunathon Owens on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AirPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
